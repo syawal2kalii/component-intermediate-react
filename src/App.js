b1f58da9193eba1712bde1ee03b4a7f915c03a2e
syawal2kalii <syawal20162016@gmail.com>
2020-06-26 21:14:26 +0800

    5_hoc_local_storage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const withUser = (WrappedComponent)=>{
-  class WithUser extends Component {
-    constructor(props){
-      super(props)
-      this.state = {
-        user : 'Syawal',
-        email : 'syawal20162016@gmail.com'
-      }
+const withLocalStorage = (WrappedComponent)=>{
+  class WithLocalStorage extends Component {
+    get = (key) => {
+      return localStorage.getItem(key)
     }
+
+    save = (key,data)=>{
+      return localStorage.setItem(key,data)
+    }
+
+    remove = (key) => {
+      return localStorage.removeItem(key)
+    }
+
     render() {
-      return <WrappedComponent user={this.state.user} email={this.state.email} {...this.props}/>
+      return <WrappedComponent get = {this.get}
+                               save = {this.save}
+                               remove = {this.remove}
+                               {...this.props} 
+      />
     }
   }
-  return WithUser
+
+  return WithLocalStorage
 }
 
-class App extends Component{
+class User extends Component{
+
+  state = {
+    name : ''
+  }  
+  
+  componentDidMount(){
+    const name = this.props.get('name')
+    
+    if (!name) {
+      this.props.save('name','user_random')
+      this.setState({
+        name : 'user_random'
+      });
+    }else{
+      this.setState({
+        name:this.props.get('name')
+      })
+    }
+  }
+
   render(){
     return (
       <div>
-        I am App {this.props.user} email {this.props.email}
+        I am {this.state.name}
       </div> 
     )
   }
 }
-export default withUser(App);
+
+export default withLocalStorage(User);
