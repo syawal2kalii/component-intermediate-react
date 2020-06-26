00af3846eaafd010f9e70cee36bcfe9fe64caa11
syawal2kalii <syawal20162016@gmail.com>
2020-06-26 16:35:58 +0800

    2_React_Context

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+const MyContext = React.createContext();
+
+class App extends Component {
+  state = {
+    color : "black",
+    info : 'warning',
+  }
+  render(){
   return (
-    <div>
-      <Welcome status='success'/>
-      <p>Konten</p>
-      <Warning status='warning' />
-    </div>
+    <MyContext.Provider value={this.state}>
+      <header>
+        <Navigation color="red" />
+      </header>
+    </MyContext.Provider>
   );
+  }
 }
 
-function Welcome() {
-  return (
-    <Notification status='success'>
-      <h1>Selamat Datang</h1>
-      <p>Selamat Bergabung</p>
-    </Notification>
-  )
+class Navigation extends Component{
+  render(){
+    return(
+      <div>
+        <a href="#">Home</a>
+        <a href="#">Kontak</a>
+
+        <Search color={this.props.color}/>
+        <Button />
+      </div>
+    )
+  }
 }
 
-function Warning() {
-  return (
-    <Notification status='warning'>
-      <p>Warning</p>
-    </Notification>
-  )
+class Search extends Component{
+  render(){
+    return (
+      <div>
+        <label>Search</label>
+        <input type="search" placeholder={this.props.color} />
+      </div>
+    )
+  }
 }
 
-function Notification(props) {
-  return(
-    <div className={"notification is-"+ props.status} >
-      {props.children}
-    </div>
-  )
+class Button extends Component{
+  render(){
+    return(
+      <MyContext.Consumer>
+        {(context)=>(
+          <button>Warna yang kita dapat adalah {context.color} & infonya {context.info}</button>
+        )}
+      </MyContext.Consumer>
+    )
+  }
 }
 
 export default App;
