3ba8a632f70adcf8f3b33bdacb0265070b968590
syawal2kalii <syawal20162016@gmail.com>
2020-06-26 23:33:41 +0800

    7_render_props

--- src/App.js
+++ src/App.js
@@ -1,63 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
 
-// context
-const AuthContext = React.createContext()
-
-// Hoc
-const withAuth = (WrappedComponent)=>{
-  class WithAuth extends Component{
-    render(){
-      return (
-        <AuthContext.Consumer>
-          {(context)=>(
-            <WrappedComponent {...this.props} {...context} />
-          )}
-        </AuthContext.Consumer>
-      )
-    }
+class Amount extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      amount : 0
+    };
   }
-  return WithAuth
+
+  onIncrement = () => {
+    this.setState(state => ({ amount : state.amount + 1}));
+  };
+
+  onDecrement = () => { 
+    this.setState(state => ({ amount : state.amount - 1 }));
+  };
+
+  render (){
+    return (
+      <div>
+        <button type="button" onClick={this.onIncrement}>+</button>
+        <button type="button" onClick={this.onDecrement}>-</button>
+        {this.props.render(this.state.amount)}
+      </div>
+    )
+  }
 }
 
 class App extends Component{
-  state = {
-    username : 'randomuser'
-  }
-
-  render(){
-    return( 
-      <AuthContext.Provider value={this.state}>
-        <h1>I'm App</h1>
-        <Navigation />
-        <Profile />
-      </AuthContext.Provider>
-    )
-  }
-}
-
-class NavigationBase extends Component{
-  render (){
-    return (
-      <nav>
-        <a href="#">Home</a>
-        <a href="/kontak">Kontak</a>
-        <br/>
-        <button> Current Login User : {this.props.username}</button>
-      </nav>
-    )
-  }
-}
-
-const Navigation = withAuth(NavigationBase);
-
-class ProfileBase extends Component{
   render(){
     return (
-      <h2>Halaman Profile : {this.props.username}</h2>
+      <Amount render={ amount => (
+        <div>
+          <Rupiah amount = {amount}/>
+          <Dollar amount = {amount * 14000}/>
+        </div>
+      )}/>
     )
   }
 }
-const Profile = withAuth(ProfileBase)
+
+function Rupiah(props) {
+  return <h1> Rupiah = {props.amount}</h1>
+}
+
+function Dollar(props) {
+  return <h1> Dollar = {props.amount}</h1>
+}
 
 export default App;
