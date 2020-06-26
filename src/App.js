71ff11468d4fcc428e080c629b43714721c6d086
syawal2kalii <syawal20162016@gmail.com>
2020-06-26 15:00:30 +0800

    1_composition_children_props

--- src/App.js
+++ src/App.js
@@ -4,23 +4,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Welcome status='success'/>
+      <p>Konten</p>
+      <Warning status='warning' />
     </div>
   );
+}
+
+function Welcome() {
+  return (
+    <Notification status='success'>
+      <h1>Selamat Datang</h1>
+      <p>Selamat Bergabung</p>
+    </Notification>
+  )
+}
+
+function Warning() {
+  return (
+    <Notification status='warning'>
+      <p>Warning</p>
+    </Notification>
+  )
+}
+
+function Notification(props) {
+  return(
+    <div className={"notification is-"+ props.status} >
+      {props.children}
+    </div>
+  )
 }
 
 export default App;
